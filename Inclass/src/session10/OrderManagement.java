c43696fbafee72e9969e91df5dbbb327ed91f746
hongquan <danghongquanvtc1111@gmail.com>
2024-05-08 19:29:54 +0700

    Update Ex Session 10

--- Inclass/src/session10/OrderManagement.java
+++ Inclass/src/session10/OrderManagement.java
@@ -12,9 +12,16 @@ public class OrderManagement {
         PreparedStatement pstm = null;
         ResultSet rs = null;
         conn.setAutoCommit(false);//đánh dấu điểm bắt đầu của transaction
+        //tắt chế độ tự động commit trên kết nối cơ sở dữ liệu.
+        // Khi bạn tắt chế độ tự động commit, các thay đổi trong cơ sở dữ liệu sẽ không được tự động lưu lại ngay
+        // sau mỗi lệnh SQL được thực thi.
+        // Thay vào đó, bạn cần phải gọi conn.commit() để lưu lại các thay đổi hoặc conn.rollback() để hủy bỏ các thay đổi.
         String orderQuery = "INSERT INTO orders(customer_id, create_at, update_at, payment_type) VALUES(?,?,?,?)";
         String orderDetailQuery = "INSERT INTO order_details(order_id, create_at, update_at,product_id, price) VALUES(?,?,?,?,?)";
         pstm = conn.prepareStatement(orderQuery, PreparedStatement.RETURN_GENERATED_KEYS);
+        // PreparedStatement.RETURN_GENERATED_KEYS: trả về các khóa sinh tự động được tạo ra khi thực hiện truy vấn.
+        // Thường được sử dụng trong các trường hợp khi bạn thêm dữ liệu vào một bảng có trường tự tăng (auto-increment),
+        // và bạn muốn lấy giá trị của trường tự tăng đó sau khi thêm dữ liệu.
         pstm.setInt(1, 1);
         pstm.setTimestamp(2, new java.sql.Timestamp(new java.util.Date().getTime()));
         pstm.setTimestamp(3, new java.sql.Timestamp(new java.util.Date().getTime()));
--- Inclass/src/session10/controller/OrderDetailController.java
+++ Inclass/src/session10/controller/OrderDetailController.java
@@ -1,4 +1,27 @@
 package session10.controller;
 
+import session10.entity.OrderDetail;
+import session10.model.Order_Detail_DAOImpl;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class OrderDetailController {
+    Order_Detail_DAOImpl OrderDetailDAO = new Order_Detail_DAOImpl();
+
+    public void addOrderDetail(OrderDetail orderDetail) throws SQLException {
+        OrderDetailDAO.addOrderDetail(orderDetail);
+    }
+
+    public boolean deleteOrderDetail(int id) throws SQLException {
+        return OrderDetailDAO.deleteOrderDetail(id);
+    }
+
+    public void updateOrderDetail(OrderDetail orderDetail) throws SQLException {
+        OrderDetailDAO.updateOrderDetail(orderDetail);
+    }
+
+    public ArrayList<OrderDetail> showOrderDetailsByCustomerID(int id) throws SQLException {
+        return OrderDetailDAO.showOrderDetailsByCustomerID(id);
+    }
 }
